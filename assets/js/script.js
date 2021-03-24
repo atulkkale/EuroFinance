6f55fb2190fbab596fdeb2182f9165bc7f3e7a71
Atul Kale <kaleatul9@gmail.com>
2021-03-24 17:03:27 +0530

    Form validation js done.

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,3 +91,130 @@ function toggleLightBox(e) { // This function open and close lightBox.
       break;
   }
 }
+
+// Form validation. 
+var form = {
+  errorCount: { 0: null, 1: null, 2: null, 3: null, 4: null, 5: null}, // If any of the form field is valid then it will set to object otherwise null and this field give referance to submit function.
+  formInputs: document.querySelectorAll('input[class=home_user_input]'),
+  validate: function (e) { // This function validate field on event keyup.
+    var regex;
+    var currelemnt = e.target;
+    // console.log(form.formInputs['2'])
+    switch (currelemnt) {
+    case form.formInputs['0']:
+      regex = [/^[a-zA-Z]{2,15}$/,2,16,'lenerror','syntaxerror']; // First name regex.
+    break;
+
+    case form.formInputs['1']:
+      regex = [/^[a-zA-Z]{2,15}$/,2,16,'lenerror','syntaxerror']; // Last name regex.
+    break;
+
+    case form.formInputs['2']:
+      regex = [/^[a-zA-Z0-9]+@[a-zA-Z]{0,7}\.[a-zA-Z]{2,5}$/,4,21,'gmaillenerror','gmailerror']; // Gmail regex.
+    break;
+
+    case form.formInputs['3']:
+      regex = [/^[0-9]{10}$/,9,11,'phonelenerror','phoneerror'];
+    break;
+    
+    case form.formInputs['4']:
+      regex = [/./,4,21,'gmaillenerror','syntaxerror'];
+    break;
+
+    case form.formInputs['5']:
+      if(form.formInputs['4'].value){
+        var regexpass = new RegExp(form.formInputs['4'].value);
+      }else{
+        var regexpass = /\B\b/;
+      }
+      regex = [regexpass,4,21,'gmaillenerror','passworderror'];
+    break;
+    
+    default:
+    break;
+    }
+    // currelemnt === form.formInputs['0']
+    //   ? (regex = [/^[a-zA-Z]{2,15}$/,2,16,'lenerror','syntaxerror']) // Regular exrpression for first name.
+    //   : (regex = [/^[a-zA-Z0-9]+@[a-zA-Z]{0,7}\.[a-zA-Z]{2,5}$/,5,20,'gmaillenerror','gmailerror']); // Regular exrpression for email.
+
+    if(!(currelemnt.value.length > regex[1] && currelemnt.value.length < regex[2])){
+      removeOrAddError(regex[3],currelemnt);
+    } else if(!(regex[0].test(currelemnt.value))){
+      removeOrAddError(regex[4],currelemnt);
+    } else {
+      removeOrAddError('validfield',currelemnt);
+    }
+  },
+  submit: function (e) {
+    // e.preventDefault(); // For form to submit data remove this line.
+    a = {};
+    for(var input in form.errorCount){
+      if(form.errorCount[input] === null){
+        e.preventDefault();
+        a.target = form.formInputs[input];
+        form.validate(a);
+      }
+    }
+  },
+};
+console.log(form.formInputs)
+function removeOrAddError(apply, element) { // This function shows and removes error massage on each fields.
+
+  var errorcode = ['lenerror','validfield','syntaxerror','gmaillenerror','gmailerror','phonelenerror','phoneerror','passworderror']; // Store all error code.
+
+  for (err of errorcode) {
+    element.parentNode.classList.remove(err);
+  }
+  element.parentNode.classList.add(apply);
+
+  switch (element) {
+    case form.formInputs[0]:
+      apply === 'validfield'
+        ? (form.errorCount['0'] = form.formInputs[0])
+        : (form.errorCount['0'] = null); // This indicates that if current field is valid or not.
+      break;
+
+    case form.formInputs[1]:
+      apply === 'validfield'
+        ? (form.errorCount['1'] = form.formInputs[1])
+        : (form.errorCount['1'] = null);
+      break;
+
+    case form.formInputs[2]:
+      apply === 'validfield'
+        ? (form.errorCount['2'] = form.formInputs[2])
+        : (form.errorCount['2'] = null);
+      break;
+
+    case form.formInputs[3]:
+      apply === 'validfield'
+        ? (form.errorCount['3'] = form.formInputs[3])
+        : (form.errorCount['3'] = null);
+      break;
+
+    case form.formInputs[4]:
+      apply === 'validfield'
+        ? (form.errorCount['4'] = form.formInputs[4])
+        : (form.errorCount['4'] = null);
+      break;
+
+    case form.formInputs[5]:
+      apply === 'validfield'
+        ? (form.errorCount['5'] = form.formInputs[5])
+        : (form.errorCount['5'] = null);
+      break;
+
+    default:
+      break;
+  }
+}
+
+for (input of form.formInputs) {
+  input.addEventListener('keyup', form.validate); // Fire validate funciton.
+  var nameattr = input.getAttribute('placeholder');
+  // console.log(nameattr,input)
+  input.parentNode.setAttribute('data-field-name', nameattr); // Set attribute to each field to show specific error message.
+}
+
+var submit = document.querySelector('input[type=submit]');
+submit.addEventListener('click', form.submit); // Fire submit function.
