15ff862dbf84f6b3dbe34dbbacec3a7bf594d3c9
Atul Kale <kaleatul9@gmail.com>
2021-03-24 10:15:55 +0530

    Scroll to top js functionality added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
+// Scroll to top.
+var topBtn = document.querySelector(".top");
+document.addEventListener('scroll', scrollToTop);
+topBtn.addEventListener('click', goTop);
 
+function scrollToTop(){
+  var scrollShowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var aspectRatio = 0.1;
+  if((document.documentElement.scrollTop / scrollShowHeight) > aspectRatio){ 
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function goTop(){
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
 
 
 
