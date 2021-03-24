f2795e51c9ac2ab32667947eda590053baa6d507
Atul Kale <kaleatul9@gmail.com>
2021-03-24 11:41:16 +0530

    Hamburger added with style.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,59 @@
 // Scroll to top.
-var topBtn = document.querySelector(".top");
+var topBtn = document.querySelector('.top');
 document.addEventListener('scroll', scrollToTop);
 topBtn.addEventListener('click', goTop);
 
-function scrollToTop(){
-  var scrollShowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var aspectRatio = 0.1;
-  if((document.documentElement.scrollTop / scrollShowHeight) > aspectRatio){ 
-    topBtn.style.display = "block";
+function scrollToTop() {
+  var scrollShowHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight; // Store scrollHeight - clientHeight.
+  var aspectRatio = 0.1; // Store aspectRatio for show or hide top button.
+  if (document.documentElement.scrollTop / scrollShowHeight > aspectRatio) {
+    topBtn.style.display = 'block';
   } else {
-    topBtn.style.display = "none";
+    topBtn.style.display = 'none';
   }
 }
 
-function goTop(){
+function goTop() {
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
-  })
+    behavior: 'smooth',
+  });
 }
 
+// Hamburger js.
+var menu = document.querySelector('.menu');
+var menuLi = document.querySelectorAll('.menu li a');
+var closeHamburger = document.querySelector('.close');
+var hamburger = document.querySelector('.hamburger');
+closeHamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
+console.log(menu);
 
+function toggleMenu() {
+  var clickItemName = this.getAttribute('class');
+  switch (clickItemName) {
+    case 'hamburger':
+      hamburger.style.display = 'none';
+      menu.classList.add('menushow');
+      closeHamburger.style.display = 'block';
 
+      window.onscroll = function () {
+        // Disable scroll.
+        window.scrollTo(0, 0);
+      };
+      break;
 
+    case 'close':
+      hamburger.style.display = 'block';
+      menu.classList.remove('menushow');
+      closeHamburger.style.display = 'none';
+      window.onscroll = function () {}; // Enable scroll.
+      break;
 
-
+    default:
+      break;
+  }
+}
